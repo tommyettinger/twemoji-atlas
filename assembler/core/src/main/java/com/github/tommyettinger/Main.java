c22c5148dab46268b2c52dfbff8c404bf1fef3f7
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-07-04 22:22:38 -0700

    A big one.

--- assembler/core/src/main/java/com/github/tommyettinger/Main.java
+++ assembler/core/src/main/java/com/github/tommyettinger/Main.java
@@ -2,6 +2,7 @@ package com.github.tommyettinger;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 
@@ -10,7 +11,16 @@ public class Main extends ApplicationAdapter {
     public void create() {
         JsonReader reader = new JsonReader();
         JsonValue json = reader.parse(Gdx.files.internal("emoji.json"));
-        int count = json.size;
-        System.out.println(count);
+//        int count = json.size;
+        for (JsonValue entry = json.child; entry != null; entry = entry.next) {
+            String codename = entry.getString("codes").toLowerCase().replace(' ', '-') + ".png";
+            String charString = entry.getString("char") + ".png";
+            String name = entry.getString("name").replace(':', ',').replace(".", "").replace("&", "and") + ".png";
+            FileHandle original = Gdx.files.local("../../individual/" + codename);
+            if (original.exists()) {
+                original.copyTo(Gdx.files.local("../../renamed/emoji/" + charString));
+                original.copyTo(Gdx.files.local("../../renamed/name/" + name));
+            }
+        }
     }
 }
