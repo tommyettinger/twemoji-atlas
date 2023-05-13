b6fb8b3be4b84a076d306a0e30ce8ade100a9235
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-05-12 20:19:12 -0700

    Almost 11,000 changed files???
    I optimized scaled-mid for file size, and edited Assembler so it automatically cleans up some un-type-able punctuation marks like smart quotes/smart apostrophes. Then I ran assembler, which copied the optimized files to their sometimes-new names.

--- assembler/core/src/main/java/com/github/tommyettinger/Main.java
+++ assembler/core/src/main/java/com/github/tommyettinger/Main.java
@@ -13,35 +13,42 @@ import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 
 public class Main extends ApplicationAdapter {
+    public static final boolean FLAG_MODE = false;
     @Override
     public void create() {
         JsonReader reader = new JsonReader();
         JsonValue json = reader.parse(Gdx.files.internal("emoji.json"));
         char[] buffer = new char[2];
-        for (JsonValue entry = json.child; entry != null; entry = entry.next) {
-            if(!"Flags (country-flag)".equals(entry.getString("category"))) continue;
+        if(FLAG_MODE) {
+            for (JsonValue entry = json.child; entry != null; entry = entry.next) {
+                if(!"Flags (country-flag)".equals(entry.getString("category"))) continue;
 
-            String codename = entry.getString("codes").toLowerCase().replace(' ', '-') + ".png";
-            String charString = entry.getString("char") + ".png";
-            String name = entry.getString("name").replace(':', ',').replace(".", "").replace("&", "and") + ".png";
-            String countryUnicode = entry.getString("char");
-            buffer[0] = (char)(countryUnicode.codePointAt(1) - 56806 + 'A');
-            buffer[1] = (char)(countryUnicode.codePointAt(3) - 56806 + 'A');
-            String countryCode = String.valueOf(buffer);
-            FileHandle original = Gdx.files.local("../../scaled-tiny/" + codename);
-            if (original.exists()) {
-                original.copyTo(Gdx.files.local("../../flags-tiny/emoji/" + charString));
-                original.copyTo(Gdx.files.local("../../flags-tiny/name/" + name));
-                original.copyTo(Gdx.files.local("../../flags-tiny/code/" + countryCode + ".png"));
+                String codename = entry.getString("codes").toLowerCase().replace(' ', '-') + ".png";
+                String charString = entry.getString("char") + ".png";
+                String name = entry.getString("name").replace(':', ',').replace(".", "").replace("&", "and") + ".png";
+                String countryUnicode = entry.getString("char");
+                buffer[0] = (char)(countryUnicode.codePointAt(1) - 56806 + 'A');
+                buffer[1] = (char)(countryUnicode.codePointAt(3) - 56806 + 'A');
+                String countryCode = String.valueOf(buffer);
+                FileHandle original = Gdx.files.local("../../scaled-tiny/" + codename);
+                if (original.exists()) {
+                    original.copyTo(Gdx.files.local("../../flags-tiny/emoji/" + charString));
+                    original.copyTo(Gdx.files.local("../../flags-tiny/name/" + name));
+                    original.copyTo(Gdx.files.local("../../flags-tiny/code/" + countryCode + ".png"));
+                }
             }
-//            String codename = entry.getString("codes").toLowerCase().replace(' ', '-') + ".png";
-//            String charString = entry.getString("char") + ".png";
-//            String name = entry.getString("name").replace(':', ',').replace(".", "").replace("&", "and") + ".png";
-//            FileHandle original = Gdx.files.local("../../scaled-mid/" + codename);
-//            if (original.exists()) {
-//                original.copyTo(Gdx.files.local("../../renamed-mid/emoji/" + charString));
-//                original.copyTo(Gdx.files.local("../../renamed-mid/name/" + name));
-//            }
+        }
+        else {
+            for (JsonValue entry = json.child; entry != null; entry = entry.next) {
+                String codename = entry.getString("codes").toLowerCase().replace(' ', '-') + ".png";
+                String charString = entry.getString("char") + ".png";
+                String name = entry.getString("name").replace(':', ',').replace('“', '\'').replace('”', '\'').replace('’', '\'').replace(".", "").replace("&", "and") + ".png";
+                FileHandle original = Gdx.files.local("../../scaled-mid/" + codename);
+                if (original.exists()) {
+                    original.copyTo(Gdx.files.local("../../renamed-mid/emoji/" + charString));
+                    original.copyTo(Gdx.files.local("../../renamed-mid/name/" + name));
+                }
+            }
         }
     }
 }
