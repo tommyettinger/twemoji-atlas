5bcae0f7f81687de9556e3974fe5b75ae6ee351f
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-05-12 21:22:44 -0700

    Early draft of HTML preview writer.

--- assembler/core/src/main/java/com/github/tommyettinger/Main.java
+++ assembler/core/src/main/java/com/github/tommyettinger/Main.java
@@ -13,13 +13,13 @@ import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 
 public class Main extends ApplicationAdapter {
-    public static final boolean FLAG_MODE = false;
+    public static final String MODE = "EMOJI_HTML";
     @Override
     public void create() {
         JsonReader reader = new JsonReader();
-        JsonValue json = reader.parse(Gdx.files.internal("emoji.json"));
-        char[] buffer = new char[2];
-        if(FLAG_MODE) {
+        if("FLAG".equals(MODE)) {
+            JsonValue json = reader.parse(Gdx.files.internal("emoji.json"));
+            char[] buffer = new char[2];
             for (JsonValue entry = json.child; entry != null; entry = entry.next) {
                 if(!"Flags (country-flag)".equals(entry.getString("category"))) continue;
 
@@ -38,7 +38,8 @@ public class Main extends ApplicationAdapter {
                 }
             }
         }
-        else {
+        else if("EMOJI".equals(MODE)) {
+            JsonValue json = reader.parse(Gdx.files.internal("emoji.json"));
             for (JsonValue entry = json.child; entry != null; entry = entry.next) {
                 String codename = entry.getString("codes").toLowerCase().replace(' ', '-') + ".png";
                 String charString = entry.getString("char") + ".png";
@@ -49,6 +50,41 @@ public class Main extends ApplicationAdapter {
                     original.copyTo(Gdx.files.local("../../renamed-mid/name/" + name));
                 }
             }
+        }
+        else if("EMOJI_HTML".equals(MODE)) {
+            JsonValue json = reader.parse(Gdx.files.internal("emoji-info.json"));
+            StringBuilder sb = new StringBuilder(4096);
+            sb.append("""
+                    <!doctype html>
+                    <html>
+                    <head>
+                    \t<title>Twemoji Preview</title>
+                    \t<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+                    \t<meta id="gameViewport" name="viewport" content="width=device-width initial-scale=1">
+                    \t<link href="styles.css" rel="stylesheet" type="text/css">
+                    </head>
+                    
+                    """);
+            sb.append("<body>\n");
+            sb.append("<h1>Twemoji Preview</h1>\n");
+            sb.append("<p>This shows all emoji supported by " +
+                      "<a href=\"https://github.com/tommyettinger/twemoji-atlas\">TwemojiAtlas</a>, " +
+                      "along with the two names each can be looked up by.</p>\n");
+            sb.append("<p>The atlases and all image assets are licensed under CC-BY 4.0, with the same " +
+                      "<a href=\"https://github.com/twitter/twemoji#attribution-requirements\">permissions granted for twemoji here</a>.</p>\n");
+            for (JsonValue entry = json.child; entry != null; entry = entry.next) {
+                String emojiChar = entry.getString("char");
+                String emojiFile = "emoji/" + emojiChar + ".png";
+                String name = entry.getString("name");
+                sb.append("\t<div>\n" +
+                          "\t\t<img src=\"" + emojiFile + "\" alt=\"" + name + "\" />\n" +
+                          "\t\t<p>" + emojiChar + "</p>\n" +
+                          "\t\t<p>" + name + "</p>\n" +
+                          "\t</div>\n");
+            }
+            sb.append("</body>\n");
+            sb.append("</html>\n");
+            Gdx.files.local("index.html").writeString(sb.toString(), false, "UTF8");
         }
     }
 }
